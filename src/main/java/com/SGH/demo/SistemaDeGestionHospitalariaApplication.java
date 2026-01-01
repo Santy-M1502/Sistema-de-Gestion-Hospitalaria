e8bd298cbba62d130fe2eccf2d4e7980c017f55b
Santy_M <santy15021502@gmail.com>
2026-01-01 18:07:36 -0300

    Base

---
+++ src/main/java/com/SGH/demo/SistemaDeGestionHospitalariaApplication.java
@@ -0,0 +1,13 @@
+package com.SGH.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeGestionHospitalariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeGestionHospitalariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SGH/demo/SistemaDeGestionHospitalariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SGH.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeGestionHospitalariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
